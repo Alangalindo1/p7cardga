15f4e14c29747d85397e102ce52e6863f1251632
ALAN_OSVALDO_GALINDO_ACEVEDO <a.21308051280475@cbtis128.edu.mx>
2024-08-14 03:30:18 +0000

    qaaaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,90 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FloatingActionButtonDemo(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class FloatingActionButtonDemo extends StatefulWidget {
+  const FloatingActionButtonDemo({Key? key}) : super(key: key);
 
+  @override
+  _FloatingActionButtonDemoState createState() =>
+      _FloatingActionButtonDemoState();
+}
+
+class _FloatingActionButtonDemoState extends State<FloatingActionButtonDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Container(
+          height: 300,
+          width: 300,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              side: BorderSide.merge(
+                  BorderSide(
+                      width: 1.5, color: Colors.grey, style: BorderStyle.solid),
+                  BorderSide(
+                      width: 1.5,
+                      color: Colors.grey,
+                      style: BorderStyle.solid)),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            color: Colors.blueAccent,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 250,
+                  width: 250,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide.merge(
+                          BorderSide(
+                              width: 1.5,
+                              color: Colors.grey,
+                              style: BorderStyle.solid),
+                          BorderSide(
+                              width: 1.5,
+                              color: Colors.grey,
+                              style: BorderStyle.solid)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text(
+                            'card galindo',
+                            style: TextStyle(
+                                color: Colors.blueAccent, fontSize: 30),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(height: 100, width: 100, child: FlutterLogo())
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
